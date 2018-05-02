87c5075d30802e9890ec87d892a0db11c2545b0c
Carlos Andres Torres <caletorres_59@hotmail.com>
2018-05-02 06:46:04 -0500

    EjerciciO dos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Camera } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  img: string = 'assets/imgs/jam.jpg';
+  constructor(public navCtrl: NavController, public cameraPlugin: Camera) {
 
   }
 
+  takePicture(): void{
+    this.cameraPlugin.getPicture({
+      quality: 100,
+      destinationType: this.cameraPlugin.DestinationType.DATA_URL,
+      encodingType: this.cameraPlugin.EncodingType.JPEG,
+      mediaType: this.cameraPlugin.MediaType.PICTURE
+    }
+    ).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.img = base64Image;
+     }, (err) => {
+       console.log(err);
+     });
+  }
 }
